a75fc91c6144371760e6db773f2ca39a201a8a5b
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-04-25 22:29:25 -0500

    Aesthetics properties

--- main1.r
+++ main1.r
@@ -7,3 +7,17 @@ ggplot(data=mpg)+geom_point(mapping=aes(x=displ, y=hwy, color=class))
 #To map an aesthetic to a variable, associate the name of the aesthetic to the name
 #of the variable inside aes(). ggplot2 will assign a unique level of the aesthetic to each unique value
 #of the variable, a process known as scaling. 
+
+
+#mapped class to the color aesthetic, size aesthetic, alpha, or shape
+ggplot(data=mpg)+geom_point(mapping=aes(x=displ, y=hwy, alpha=class))
+
+#For each aesthetic, you use aes() to associate the name of the aesthetic with a variable to display
+#the x and y locations of a point are themselves aesthetics, visual properties that you can map to display
+#information about the data
+
+#You can also set aesthetics properties of your geom manually
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy, stroke=class), color = "blue")
+
+#stroke adjusts the thickness of the border for shapes that can take on different colors both inside and outside. 
+#It only works for shapes 21-24.
